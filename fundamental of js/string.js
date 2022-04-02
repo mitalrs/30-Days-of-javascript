6097b1b9ccf4e0b5ddedf6ca88e5fc323e3041ad
mitalrs <mitalsapkale1999@gmail.com>
2022-04-02 18:46:05 +0530

    day2

---
+++ fundamental of js/string.js
@@ -0,0 +1,12 @@
+let usarename="per"
+let massage="you have three notifications"
+console.log(massage+", "+usarename+"!")
+
+let messageToUser=massage+", "+usarename+"!"
+console.log(messageToUser)
+
+let name=" marrinette"
+let greeting="hi, my name is"
+let mygreeting= greeting + name
+console.log(mygreeting)
+
--- passenger counter/sourcecode.js
+++ passenger counter/sourcecode.js
@@ -1,3 +1,18 @@
+let personCount=document.getElementById("person-count")
+console.log(personCount)
+
+let count=0
 function increement(){
-    console.log("clicked")
+    count=count+1
+    personCount.innerText=count
+    console.log(count)
 }
+
+
+// function increement(){
+//     console.log("clicked")
+// }
+function save(){
+    console.log(count)
+}
+ 
