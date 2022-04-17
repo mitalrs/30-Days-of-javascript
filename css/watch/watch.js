5054f3b5784c3ea3ce6de17b9490914bb1c77399
mitalrs <mitalsapkale1999@gmail.com>
2022-04-17 17:39:31 +0530

    day11

---
+++ css/watch/watch.js
@@ -0,0 +1,12 @@
+var now = new Date();
+console.log(now)
+
+// var time = now.getHours() * 3600 +
+// 					    now.getMinutes() * 60 +
+// 					    now.getSeconds() * 1 +
+// 					    now.getMilliseconds() / 1000;
+
+//                         console.log(time)
+
+var time = now.getHours()
+console.log(time)
