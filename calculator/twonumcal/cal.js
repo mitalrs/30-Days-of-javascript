f31ff3a5e1d64450968e7f5ba4de5ddffa10f4f2
mitalrs <mitalsapkale1999@gmail.com>
2022-05-13 12:24:13 +0530

    day 13

--- calculator/twonumcal/cal.js
+++ calculator/twonumcal/cal.js
@@ -7,23 +7,43 @@ let num2=5
 let total=document.getElementById("total")
 console.log(operator)
 
+var v="0";
 
 function add(){
     operator.textContent="+" 
-    total.textContent=8+5
+    // total.textContent=8+5
+     return v ="+";
 }
 function sub(){
     operator.textContent="-" 
-    total.textContent=8-5
+    // total.textContent=8-5
+    return v ="-";
 }
 function div(){
     operator.textContent="/" 
-    total.textContent=8/5
+    // total.textContent=8/5
+    return v ="/";
 }
 function mul(){
     operator.textContent="*" 
-    total.textContent=8*5
+    // total.textContent=8*5
+    return v ="*";
 }
 function clean(){
+    operator.textContent="_" 
     total.textContent=0
+}
+function caltotal(){
+    if(v=="+"){
+        total.textContent=8+5
+    }
+    if(v=="-"){
+        total.textContent=8-5
+    }
+    if(v=="*"){
+        total.textContent=8*5
+    }
+    if(v=="/"){
+        total.textContent=8/5
+    }
 }
