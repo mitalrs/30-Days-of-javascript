3ac89e817ad53143d9a156564473f4e2f7253159
mitalrs <mitalsapkale1999@gmail.com>
2022-06-14 15:29:35 +0530

    day 19

---
+++ blackJack game/bjindex/Object/object.js
@@ -0,0 +1,22 @@
+let course={
+    title:"Learn Grid for free",
+    lessons:16,
+    creator:"hearaid borgen",
+    level:2,
+    length:63,
+    isFree:true,
+    tags:["html", "css"]
+}
+console.log(course.length)
+console.log(course["tags"])
+
+
+let airbnbCastle={
+    castleName:"Monselice, Italy",
+    bookNow:158,
+    available:true,
+    similarAnother:["Easkey, Ireland", "Rions, France","Kilmartin, Scotland"]
+}
+
+console.log(airbnbCastle.bookNow)
+console.log(airbnbCastle.available)
--- blackJack game/bjindex/mathrandom_method/logicaloperator.js
+++ blackJack game/bjindex/mathrandom_method/logicaloperator.js
@@ -1,8 +1,36 @@
 let hasCompletedCourse = true
 let givensCertificate = true
 
-
+if(hasCompletedCourse===true && givensCertificate===true){
+    generateCertificate()
+}
 function generateCertificate()
 {
-    
+    console.log("Generste the certificate.......")
 }
+
+
+let hasSolvedChallenge = false 
+let hasHintLeft = false
+
+if(hasSolvedChallenge===false && hasHintLeft===false){
+    showSolution()
+}
+
+function showSolution(){
+    console.log("showing solution")
+}
+
+
+// or operato ||
+let likesDocumenteries = false
+let likesStartup = true
+
+if(likesDocumenteries===true || likesStartup===true){
+    recommendedMovie()
+}
+
+function recommendedMovie(){
+    console.log("Hey, Check out this film we think you will like!!")
+}
+
