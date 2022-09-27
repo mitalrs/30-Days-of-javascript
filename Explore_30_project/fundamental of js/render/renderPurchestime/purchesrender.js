ef21b8dfe691a0c7a18f5348bcaded572365b945
mitalrs <mitalsapkale1999@gmail.com>
2022-09-27 23:08:09 +0530

    tailwind css

--- Explore_30_project/fundamental of js/render/renderPurchestime/purchesrender.js
+++
@@ -1,7 +0,0 @@
-let error=document.getElementById("error")
-console.log(error)
-
-let text="Somthing went wrong, plese try again"
-function button(){
-    error.textContent+=text
-}
--- Explore_30_project/fundamental of js/render/rendermsg.js
+++
@@ -1,9 +0,0 @@
-let welCom=document.getElementById("wel-com")
-console.log(welCom)
-
-let name="marrinette"
-let greeting="welcom back"
-
-welCom.innerText=greeting+" "+name
-// document.querySelector(".wel").innerHTML="👋";
-welCom.innerText=welCom.innerText+"👋" //+= always use this
