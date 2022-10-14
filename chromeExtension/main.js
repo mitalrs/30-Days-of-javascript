8da95dec914a9ddb6d4d508b521a2efc6dfbec2f
mitalrs <mitalsapkale1999@gmail.com>
2022-10-14 12:34:18 +0530

    create a folder for chrome Extension, with basic html css js file

---
+++ chromeExtension/main.js
@@ -0,0 +1,10 @@
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+
+let saveclick = document.getElementById("input-btn")
+
+saveclick.addEventListener("click", function(){
+    // console.log("save")
+    myLeads.push("www.awesomelead.com")
+    // console.log(myLeads)
+})
