48568e032a5c6c768183d37e806de6c9176d6282
mitalrs <mitalsapkale1999@gmail.com>
2022-10-15 16:40:57 +0530

    completed Chrome-Extension :)

--- chromeExtension/main.js
+++ chromeExtension/main.js
@@ -1,10 +1,67 @@
+//chrome://extensions/
+
 let myLeads = []
-let inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
+const saveclick = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-let saveclick = document.getElementById("input-btn")
 
-saveclick.addEventListener("click", function(){
-    // console.log("save")
-    myLeads.push("www.awesomelead.com")
-    // console.log(myLeads)
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+// const tabs = [
+//     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+// ]
+
+tabBtn.addEventListener("click", function(){
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+
+    // console.log(tabs[0])
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
+
+function render(leads) {
+    let listItems = " "
+    for (let i = 0; i < leads.length; i++) {
+        
+        listItems += `
+        <li>
+            <a href='${leads[i]} ' target='_blanck'>
+                ${leads[i]} 
+            </a>
+        </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+saveclick.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    console.log( localStorage.getItem("myLeads") )
+})
+
