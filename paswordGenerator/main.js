79ae9a0fc0dbb31754c3712837cdfa46a138f806
mitalrs <mitalsapkale1999@gmail.com>
2022-10-18 18:56:26 +0530

    add css in to password-generetor

--- paswordGenerator/main.js
+++ paswordGenerator/main.js
@@ -1,33 +1,40 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
-    let pswstr = 0
+    let pswstr = ""
     let rnd = 0
-    let pswstr2 = 0
+    let pswstr2 = ""
     let rnd2 = 0
 
 let ps1 = document.getElementById("ps1")
 let ps2 = document.getElementById("ps2")
+const copyBtn1 = document.getElementById("copy-btn1")
+const copyBtn2 = document.getElementById("copy-btn2")
 
-// rnd = Math.floor(Math.random() * characters.length)
-// console.log(characters[rnd])
+
 
 function pswGeneratare(){
+    pswstr = ""
+    pswstr2 = ""
     for (let i = 0; i < 15; i++) {
         rnd = Math.floor(Math.random() * characters.length)
         pswstr+= characters[rnd]
-        // console.log(i+" "+characters[rnd])
-       
     }
-    // console.log(pswstr)
-    ps1.innerText = pswstr
 
+    ps1.innerText = pswstr
+    
+    
     for (let i = 0; i < 15; i++) {
         rnd2 = Math.floor(Math.random() * characters.length)
         pswstr2+= characters[rnd2]
-        // console.log(i+" "+characters[rnd])
-       
     }
-    // console.log(pswstr)
     ps2.innerText = pswstr2
     
 }
+
+copyBtn1.addEventListener("click", function(){
+    navigator.clipboard.writeText(ps1.innerText);
+})
+
+copyBtn2.addEventListener("click", function(){
+    navigator.clipboard.writeText(ps2.innerText);
+})
