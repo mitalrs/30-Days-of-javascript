e7fe0092428be5472089b96b8b9e5bdc82d809ec
mitalrs <mitalsapkale1999@gmail.com>
2022-10-27 13:57:35 +0530

    sovlved a data fetching problem

--- CatFacts/main.js
+++ CatFacts/main.js
@@ -4,9 +4,11 @@ const factBox =document.getElementById('fact-box')
 
 
 fetch('https://catfact.ninja/fact')
-.then(response => response.json())
-.then(data => console.log(data))
+.then((response) =>{
+return response.json()})
+.then((data) =>{ console.log(data)
+   
+factBox.innerText = data.fact;
 
+})
 
-// factBox.innerText = data.fact;
-console.log(data.fact);
