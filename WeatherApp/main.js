8c2cd4a6c2d4d419550a2090169ccf4e7dd96e76
mitalrs <mitalsapkale1999@gmail.com>
2022-11-27 00:02:06 +0530

    fetch data in current forcast section successfully

---
+++ WeatherApp/main.js
@@ -0,0 +1,26 @@
+const API_Key ="1674fe49ce8caad4acdd3c2c5c332c8a";
+
+const getCurrentweatherData = async()=>{
+    const city= "pune";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`);
+    return response.json()
+}
+
+// const formatTmprature = (temp)=>`${temp?.toFixed(1)}`
+
+const loadCurrentForcast = ({name, main:{temp, temp_max, temp_min}, weather:[{description}]})=>{
+    const currentForcastElement = document.querySelector("#current-forcast")
+    currentForcastElement.querySelector(".city").textContent = name;
+    currentForcastElement.querySelector(".temp").textContent = temp;
+    currentForcastElement.querySelector(".description").textContent = description;
+    currentForcastElement.querySelector(".min-max-temp").textContent = `H; ${temp_max} L:${temp_min}`;
+    // <h1>City Name</h1>
+    //             <p class="temp">Tem</p>
+    //             <p class="description">Dscription</p>
+    //             <p class="min-max-temp">High Low</p>
+}
+
+document.addEventListener("DOMContentLoaded", async()=>{
+  const currentWeather = await  getCurrentweatherData();
+  loadCurrentForcast(currentWeather)
+})
